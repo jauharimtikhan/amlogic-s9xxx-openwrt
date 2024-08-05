7e4f60b92d97ac1207687e3b962fbbafc90574cf
jauhar-imtikhan <bangjoe.muns@gmail.com>
2024-08-06 04:39:11 +0700

    update config lede-master

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -32,6 +32,50 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 #
+# Set Default Password
+sed -i 's|root::0:0:99999:7:::|root:$6$abc123$zYX1z9A6TLP63a7s3O.VziPU5y6WbbM.XgJxN7.yKDkKmYh08s/1YJ7UbjOoCnA8U2eyjIqZB7i29GO18L1:18993:0:99999:7:::/g' package/base-files/files/etc/shadow
+#
+# SSID
+sed -i "s/lede-master/JOE-WRT/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+sed -i "s/+luci-theme-material //" feeds/luci/collections/luci/Makefile
+
+rm -rf ./package/emortal/default-settings/files/openwrt_banner
+svn export https://github.com/jauharimtikhan/openwrt/trunk/banner package/emortal/default-settings/files/openwrt_banner
+
+
+mkdir -p files/etc/openclash/core
+CLASH_DEV_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/dev | grep download_url | grep clash-linux-arm64 | awk -F '"' '{print $4}')
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep clash-linux-arm64 | awk -F '"' '{print $4}')
+CLASH_META_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/meta | grep download_url | grep clash-linux-arm64 | awk -F '"' '{print $4}')
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+chmod +x files/etc/openclash/core/clash*
+
+# costumize openclash
+cat << EOF > feeds/luci/applications/luci-app-openclash/luasrc/view/openclash/editor.htm
+<%+header%>
+<div class="cbi-map">
+<iframe id="editor" style="width: 100%; min-height: 100vh; border: none; border-radius: 2px;"></iframe>
+</div>
+<script type="text/javascript">
+document.getElementById("editor").src = "http://" + window.location.hostname + "/tinyfilemanager/index.php?p=etc/openclash";
+</script>
+<%+footer%>
+EOF
+sed -i "s/yacd/Yet Another Clash Dashboard/g" feeds/luci/applications/luci-app-openclash/root/usr/share/openclash/ui/yacd/manifest.webmanifest
+sed -i '94s/80/90/g' feeds/luci/applications/luci-app-openclash/luasrc/controller/openclash.lua
+sed -i '94 i\	entry({"admin", "services", "openclash", "editor"}, template("openclash/editor"),_("Config Editor"), 80).leaf = true' feeds/luci/applications/luci-app-openclash/luasrc/controller/openclash.lua
+
+# speedtest
+mkdir -p files/bin
+wget -qO- https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-aarch64.tgz | tar xOvz > files/bin/speedtest
+chmod +x files/bin/speedtest
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
