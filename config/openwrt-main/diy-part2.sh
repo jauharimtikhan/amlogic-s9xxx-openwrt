b334fa5241b8d8619432668f0d242fa087c0cefb
Jauhar Imtikhan <83892965+jauharimtikhan@users.noreply.github.com>
2024-08-11 19:03:44 +0700

    update init-seeting config

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -9,7 +9,7 @@
 # ------------------------------- Main source started -------------------------------
 #
 # Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$6$abc123$zYX1z9A6TLP63a7s3O.VziPU5y6WbbM.XgJxN7.yKDkKmYh08s/1YJ7UbjOoCnA8U2eyjIqZB7i29GO18L1::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
@@ -25,6 +25,34 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+sleep 5
+cd /www && wget https://github.com/noct99/blog.vpngame.com/raw/main/tinyfm.zip
+sleep 7
+cd /www
+unzip tinyfm.zip && rm tinyfm.zip
+cd www/tinyfm && ln -s / rootfs
+
+sleep 10
+
+cat <<'EOF' >/usr/lib/lua/luci/controller/tinyfm.lua
+module("luci.controller.tinyfm", package.seeall)
+function index()
+entry({"admin","system","tinyfm"}, template("tinyfm"), _("File Manager"), 45).leaf=true
+end
+EOF
+
+sleep 11
+
+cat <<'EOF' >/usr/lib/lua/luci/view/tinyfm.htm
+<%+header%>
+<div class="cbi-map"><br>
+<iframe id="tinyfm" style="width: 100%; min-height: 100vh; border: none; border-radius: 2px;"></iframe>
+</div>
+<script type="text/javascript">
+document.getElementById("tinyfm").src = window.location.protocol + "//" + window.location.host + "/tinyfm/tinyfm.php";
+</script>
+<%+footer%>
+EOF
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
