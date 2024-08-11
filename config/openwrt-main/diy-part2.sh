50c1523173d2120f4de1a2650142dc24989d4b01
jauhar-imtikhan <bangjoe.muns@gmail.com>
2024-08-11 22:46:44 +0700

    x.x

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -25,34 +25,7 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-sleep 5
-cd /www && wget https://github.com/noct99/blog.vpngame.com/raw/main/tinyfm.zip
-sleep 7
-cd /www
-unzip tinyfm.zip && rm tinyfm.zip
-cd www/tinyfm && ln -s / rootfs
 
-sleep 10
-
-cat <<'EOF' >/usr/lib/lua/luci/controller/tinyfm.lua
-module("luci.controller.tinyfm", package.seeall)
-function index()
-entry({"admin","system","tinyfm"}, template("tinyfm"), _("File Manager"), 45).leaf=true
-end
-EOF
-
-sleep 11
-
-cat <<'EOF' >/usr/lib/lua/luci/view/tinyfm.htm
-<%+header%>
-<div class="cbi-map"><br>
-<iframe id="tinyfm" style="width: 100%; min-height: 100vh; border: none; border-radius: 2px;"></iframe>
-</div>
-<script type="text/javascript">
-document.getElementById("tinyfm").src = window.location.protocol + "//" + window.location.host + "/tinyfm/tinyfm.php";
-</script>
-<%+footer%>
-EOF
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
